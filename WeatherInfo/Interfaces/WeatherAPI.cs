f919df09d47087ad4534b1914fe94dfd59455dbf
defrid <defrid73@gmail.com>
2014-07-01 03:18:35 +0400

    базовые интерфейсы

---
+++ WeatherInfo/Interfaces/WeatherAPI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace WeatherInfo.Interfaces
+{
+    /// <summary>
+    /// public должны быть только конструктор и описанный метод, всё остальное private и в целом неважно как будет устроено
+    /// </summary>
+    interface WeatherAPI
+    {
+        //тут должен быть конструктор, здесь по идее подключение к серверу
+
+        XmlDocument getWeek();
+        /*здесь получаем XML с прогнозом на неделю, 
+         * при этом он подробный, а значит отдельно по дням получать уже не нужно
+         * пока что это будет XMLDocument, позже тип может смениться, но суть будет одна - XML*/
+    }
+}
---
+++ WeatherInfo/Interfaces/XMLWorker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeatherInfo.Interfaces
+{
+    /// <summary>
+    /// тут будет обработка xml, и отсюда вызывается API
+    /// все методы будут возвращать массив строк, где:
+    /// [0] средняя температура
+    /// [1] минимальная
+    /// [2] максимальная
+    /// [3] облачность
+    /// </summary>
+    interface XMLWorker
+    {
+        //тут конструктор, принимающий город
+
+        string[] getHour(int hour);//инфа за один час(отрезок времени, вроде как минимум - 3 часа)
+
+        string[] getDay(int day);//здесь парсится инфа по часам за заданный день
+
+        string[] getWeek();//здесь вся неделя
+
+        void update();//полное обновление текущего xml
+    }
+}
