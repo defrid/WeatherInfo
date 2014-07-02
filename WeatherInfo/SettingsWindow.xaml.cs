b980fd7059d1163d4048b420c6fe62db46f4de2a
Danila Matyushin <d.matyushin@gmail.com>
2014-07-02 10:14:24 +0400

    Дополнил обработчики на комбобоксах

--- WeatherInfo/SettingsWindow.xaml.cs
+++ WeatherInfo/SettingsWindow.xaml.cs
@@ -28,12 +28,10 @@ namespace WeatherInfo
         //Settings settings;
         public string country = "Россия,Англия,Турция";
         public string city = "Ульяновск,Москва,Димитровград";
-        //public string delay = "10,15,20,30";
-        //public string format = "по неделям,по дням,по часам";
 
         private void cancel_btn_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void about_btn_Click(object sender, RoutedEventArgs e)
@@ -46,29 +44,50 @@ namespace WeatherInfo
 
         public string country_save = "";
         public string city_save = "";
-        public string delay_save = "";//Delay.slightDelay;
-        public string format_save = "";//FormatForecast.Days;
+        public string delay_save = "";
+        public string format_save = "";
         public bool autostart_save = true;
 
         private void accept_btn_Click(object sender, RoutedEventArgs e)
         {
-            country_save = listOfCountries_cbx.Text.ToString();
-            city_save = listOfCitiies_cbx.Text.ToString();
-            delay_save = Enum.GetName(typeof(Delay), int.Parse(listOfVariablesDelay_cbx.Text));//(Delay)Enum.Parse(typeof(Delay), listOfVariablesDelay_cbx.Text.ToString());
-            format_save = SettingsHandler.GetValueByAttribute(listOfFormatsForecast_cbx.Text.ToString());//Enum.GetName(typeof(FormatForecast), SettingsHandler.GetValueByAttribute(listOfFormatsForecast_cbx.Text.ToString()));//(FormatForecast)Enum.Parse(typeof(FormatForecast), SettingsHandler.GetValueByAttribute(listOfFormatsForecast_cbx.Text.ToString()));
+            country_save = listOfCountries_cbx.SelectedItem.ToString();
+            city_save = listOfCitiies_cbx.SelectedItem.ToString();
+            delay_save = Enum.GetName(typeof(Delay), int.Parse(listOfVariablesDelay_cbx.SelectedItem.ToString()));
+            format_save = SettingsHandler.GetValueByAttribute(listOfFormatsForecast_cbx.SelectedItem.ToString());
             autostart_save = (bool)autostartFlag_chbx.IsChecked;
 
             App.settings = new Settings(country_save, city_save, format_save, delay_save, autostart_save);
 
             SettingsHandler.WriteXml(App.settings);
-
-            //MessageBox.Show(country_save + " " + city_save + " " + format_save + " " + delay_save);
+            Autorun();
+            Close();
         }
 
-        bool flag = false;
+        /// <summary>
+        /// Метод утановки автозагрузки приложения.
+        /// ВАЖНО: просто наткнулся в сэмплах, позже разобраться, сейчас пусть повисит закомменченным
+        /// </summary>
+        private void Autorun()
+        {
+            /*if ((bool)autostartFlag_chbx.IsChecked)
+            {
+                Microsoft.Win32.RegistryKey Key =
+                Microsoft.Win32.Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\", true);
+                Key.SetValue("WeatherInfo", "\\WeatherInfo.exe");
+                Key.Close();
+            }
+            else
+            {
+                Microsoft.Win32.RegistryKey key =
+                Microsoft.Win32.Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                key.DeleteValue("WeatherInfo", false);
+                key.Close();
+            }*/
+        }
+
         private void autostartFlag_chbx_Checked(object sender, RoutedEventArgs e)
         {
-            flag = (bool)autostartFlag_chbx.IsChecked;//true;
+            autostart_save = (bool)autostartFlag_chbx.IsChecked;//true;
             //MessageBox.Show("Автозапуск");
         }
 
@@ -81,30 +100,54 @@ namespace WeatherInfo
                 listOfCountries_cbx.Items.Add(countries[i]);
             }
             listOfCountries_cbx.SelectedItem = App.settings.country;
+            listOfCountries_cbx.SelectionChanged += listOfCountries_cbx_SelectionChanged;
 
             for (int i = 0; i < cities.Length; i++)
             {
                 listOfCitiies_cbx.Items.Add(cities[i]);
             }
             listOfCitiies_cbx.SelectedItem = App.settings.city;
+            listOfCitiies_cbx.SelectionChanged += listOfCitiies_cbx_SelectionChanged;
 
-            string[] delays = Enum.GetNames(typeof(Delay));//delay.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] delays = Enum.GetNames(typeof(Delay));
             foreach (var d in delays)
             {
                 int value = (int)Enum.Parse(typeof(Delay), d);
                 listOfVariablesDelay_cbx.Items.Add(value);
             }
             listOfVariablesDelay_cbx.SelectedItem = (int)Enum.Parse(typeof(Delay), App.settings.delay);
+            listOfVariablesDelay_cbx.SelectionChanged += listOfVariablesDelay_cbx_SelectionChanged;
 
-            string[] formats = Enum.GetNames(typeof(FormatForecast));//format.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] formats = Enum.GetNames(typeof(FormatForecast));
             foreach (var f in formats)
             {
                 var value = SettingsHandler.GetFormatAttribute(f);
                 listOfFormatsForecast_cbx.Items.Add(value);
             }
             listOfFormatsForecast_cbx.SelectedItem = SettingsHandler.GetFormatAttribute(App.settings.format);
+            listOfFormatsForecast_cbx.SelectionChanged += listOfFormatsForecast_cbx_SelectionChanged;
 
             autostartFlag_chbx.IsChecked = App.settings.autostart;
-        }        
+        }
+
+        private void listOfCountries_cbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            country_save = Enum.GetName(typeof(Delay), int.Parse(listOfCountries_cbx.SelectedItem.ToString()));
+        }
+
+        private void listOfCitiies_cbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            city_save = Enum.GetName(typeof(Delay), int.Parse(listOfCitiies_cbx.SelectedItem.ToString()));
+        }
+        
+        private void listOfVariablesDelay_cbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            delay_save = Enum.GetName(typeof(Delay), int.Parse(listOfVariablesDelay_cbx.SelectedItem.ToString()));
+        }
+
+        private void listOfFormatsForecast_cbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            format_save = Enum.GetName(typeof(Delay), int.Parse(listOfFormatsForecast_cbx.SelectedItem.ToString()));
+        }
     }
 }
